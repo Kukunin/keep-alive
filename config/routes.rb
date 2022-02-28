49c1328deefc306b5037c8eaff6113d6ecf8b8e6
Sergiy Kukunin <sergey.kukunin@gmail.com>
2022-02-27 18:09:34 -0800

    Make rubocop more or less happy

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
-
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   resources :notifications, only: [:index], export: true
   resources :announcements, only: [:index], export: true
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
